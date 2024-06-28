ac1d77813346bc759bc994134e2bba74d57f49e1
Kyoung-Seop Park <kyoung-seoppark@Kyoung-Seopui-MacBookPro.local>
2024-06-28 16:28:25 +0900

    배치 실패 시, 젠킨스에서도 실패 처리가 되도록 system exit를 하게 함

--- src/main/kotlin/my/kopring/batch/BatchSettingApplication.kt
+++ src/main/kotlin/my/kopring/batch/BatchSettingApplication.kt
@@ -1,11 +1,21 @@
 package my.kopring.batch
 
+import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import kotlin.system.exitProcess
 
 @SpringBootApplication
 class BatchSettingApplication
 
 fun main(args: Array<String>) {
-	runApplication<BatchSettingApplication>(*args)
+	var exitCode = 0
+
+	try {
+		exitCode = SpringApplication.exit(runApplication<BatchSettingApplication>(*args))
+	} catch (e: Exception) {
+		exitCode = 5
+	} finally {
+		exitProcess(exitCode)
+	}
 }
